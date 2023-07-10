ead54fbed4d5e16be8a1a85a44a69e525507602f
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-07-10 10:40:52 +0200

    Character List View: Design of the CollectionView

--- Rick-And-Morty-App/ViewModels/CharacterListViewViewModel.swift
+++ Rick-And-Morty-App/ViewModels/CharacterListViewViewModel.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import UIKit
 
-struct CharacterListViewViewModel {
+final class CharacterListViewViewModel: NSObject {
     func fetchCharacters() {
         RMService.shared.execute(.listCharacterRequests, expecting: RMGetAllCharactersResponse.self) { result in
             switch result {
@@ -18,5 +19,23 @@ struct CharacterListViewViewModel {
                 print(String(describing: error))
             }
         }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let widthCell = (bounds.width-30)/2
+        return CGSize(width: widthCell, height: widthCell * 1.5)
     }
 }
