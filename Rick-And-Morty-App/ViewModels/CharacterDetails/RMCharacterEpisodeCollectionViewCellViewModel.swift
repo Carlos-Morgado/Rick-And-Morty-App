4d0b4922181403a8c809aa843090bea902ee4eec
Carlos Morgado <cmorgadoalarcon@gmail.com>
2023-07-25 09:10:22 +0200

    Character Detail View: Character information view models

--- Rick-And-Morty-App/ViewModels/CharacterDetails/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ Rick-And-Morty-App/ViewModels/CharacterDetails/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 final class RMCharacterEpisodeCollectionViewCellViewModel {
     private let episodeDataUrl: URL?
+    
     init(episodeDataUrl: URL?) {
         self.episodeDataUrl = episodeDataUrl
     }
--- Rick-And-Morty-App/Views/CharacterDetails/RMCharacterInformationCollectionViewCell.swift
+++ Rick-And-Morty-App/Views/CharacterDetails/RMCharacterInformationCollectionViewCell.swift
@@ -12,8 +12,8 @@ final class RMCharacterInformationCollectionViewCell: UICollectionViewCell {
     
     private let valueLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Earth"
         label.font = .systemFont(ofSize: 22, weight: .light)
         return label
     }()
@@ -21,7 +21,6 @@ final class RMCharacterInformationCollectionViewCell: UICollectionViewCell {
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Location"
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 20, weight: .medium)
         return label
@@ -30,7 +29,6 @@ final class RMCharacterInformationCollectionViewCell: UICollectionViewCell {
     private let iconImageView: UIImageView = {
         let icon = UIImageView()
         icon.translatesAutoresizingMaskIntoConstraints = false
-        icon.image = UIImage(systemName: "globe.americas")
         icon.contentMode = .scaleAspectFit
         return icon
     }()
@@ -70,8 +68,8 @@ final class RMCharacterInformationCollectionViewCell: UICollectionViewCell {
             
             valueLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
             valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
-            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
-            valueLabel.heightAnchor.constraint(equalToConstant: 30),
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            valueLabel.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor),
             
             iconImageView.heightAnchor.constraint(equalToConstant: 30),
             iconImageView.widthAnchor.constraint(equalToConstant: 30),
@@ -85,10 +83,16 @@ final class RMCharacterInformationCollectionViewCell: UICollectionViewCell {
         valueLabel.text = nil
         titleLabel.text = nil
         iconImageView.image = nil
+        titleLabel.textColor = .label
+        iconImageView.tintColor = .label
         
     }
     
     func configure(with viewModel: RMCharacterInformationCollectionViewCellViewModel) {
-        
+        titleLabel.text = viewModel.title
+        titleLabel.textColor = viewModel.tintColor
+        valueLabel.text = viewModel.displayValue
+        iconImageView.image = viewModel.iconImage
+        iconImageView.tintColor = viewModel.tintColor
     }
 }
